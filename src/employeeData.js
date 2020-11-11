e1d71275480432d4659b24b653ac8266bda7c6e5
Jason Chitla <jasonchitla@hotmail.com>
2020-11-11 15:11:34 -0500

    added the mock data

---
+++ src/employeeData.js
@@ -0,0 +1,28 @@
+module.exports = {
+    employees: [
+        {
+            id: 213354,
+            firstName: 'Jason',
+            lastName: 'Chitla',
+            email: 'jasonchitla@hotmail.com'
+        },
+        {
+            id: 504159,
+            firstName: 'Venkatesh',
+            lastName: 'Yerramsetty',
+            email: 'vyerramsetty@fidelity.com'
+        },
+        {
+            id: 631450,
+            firstName: 'Sancho',
+            lastName: 'Sebastine',
+            email: 'ssebastine@fidelity.com'
+        },
+        {
+            id: 557929,
+            firstName: 'Subin',
+            lastName: 'John',
+            email: 'sjohn@fidelity.com'
+        }
+    ]
+};
