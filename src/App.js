cc36f5a22e8fbf7a31a60f0744d4d432264edcaf
Jason Chitla <jasonchitla@hotmail.com>
2020-11-11 15:11:56 -0500

    added react table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import React from "react";
+import employeeData from './employeeData'
+import Table from './Table'
+  
+const columns = [
+  {
+    Header: "ID",
+    accessor: "id"
+  },
+  {
+    Header: "First Name",
+    accessor: "firstName"
+  },
+  {
+    Header: "Last Name",
+    accessor: "lastName"
+  },
+  {
+    Header: "Email",
+    accessor: "email"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <div className="header-container">
+          <h1>Employees</h1>
+          <button className="header-button" >Add +</button>
+        </div>
+        <Table columns={columns} data={employeeData.employees} />
       </header>
     </div>
   );
